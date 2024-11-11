d80a696e8974004a5d796041fcd5f00c881b128e
SilviaUvA <silviaabbring@gmail.com>
2024-11-11 09:02:00 +0100

    reranker with a default model

--- benchmark_beir.py
+++ benchmark_beir.py
@@ -1,4 +1,6 @@
 import os
+import pathlib
+import logging
 import torch
 import sys
 
@@ -8,6 +10,8 @@ try:
     from beir.datasets.data_loader import GenericDataLoader
     from beir.retrieval.evaluation import EvaluateRetrieval
     from beir.retrieval.search.lexical import BM25Search as BM25
+    from beir.reranking.models import CrossEncoder
+    from beir.reranking import Rerank
 except ImportError:
     import subprocess
     subprocess.check_call([sys.executable, "-m", "pip", "install", "beir"])
@@ -15,6 +19,8 @@ except ImportError:
     from beir.datasets.data_loader import GenericDataLoader
     from beir.retrieval.evaluation import EvaluateRetrieval
     from beir.retrieval.search.lexical import BM25Search as BM25
+    from beir.reranking.models import CrossEncoder
+    from beir.reranking import Rerank
 
 import random
 import numpy as np
@@ -248,8 +254,6 @@ def main():
     #     )
     #     logger.info(time.time() - tick)
 
-    import pathlib, os
-    import logging
 
     #### Just some code to print debug information to stdout
     logging.basicConfig(format='%(asctime)s - %(message)s',
@@ -299,9 +303,36 @@ def main():
     #### Retrieve dense results (format of results is identical to qrels)
     results = retriever.retrieve(corpus, queries)
 
+    ################################################
+    #### (2) RERANK Top-100 docs using Cross-Encoder
+    ################################################
+
+    #### Reranking using Cross-Encoder models #####
+    #### https://www.sbert.net/docs/pretrained_cross-encoders.html
+    cross_encoder_model = CrossEncoder('cross-encoder/ms-marco-electra-base') #TODO load custom CE in here
+    reranker = Rerank(cross_encoder_model, batch_size=128)
+
+    # Rerank top-100 results using the reranker provided
+    rerank_results = reranker.rerank(corpus, queries, results, top_k=100)
+
     #### Evaluate your retrieval using NDCG@k, MAP@K ...
+    ndcg, _map, recall, precision = EvaluateRetrieval.evaluate(qrels, rerank_results, retriever.k_values)
+
+    #### Print top-k documents retrieved ####
+    top_k = 10
+
+    query_id, ranking_scores = random.choice(list(rerank_results.items()))
+    scores_sorted = sorted(ranking_scores.items(), key=lambda item: item[1], reverse=True)
+    logging.info("Query : %s\n" % queries[query_id])
+
+    # for rank in range(top_k):
+    #     doc_id = scores_sorted[rank][0]
+    #     # Format: Rank x: ID [Title] Body
+    #     logging.info("Rank %d: %s [%s] - %s\n" % (rank+1, doc_id, corpus[doc_id].get("title"), corpus[doc_id].get("text")))
+
+    ### Evaluate your retrieval using NDCG@k, MAP@K ...
     logging.info("Retriever evaluation for k in: {}".format(retriever.k_values))
-    ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
+    # ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
 
 
 if __name__ == "__main__":
