41f28769ffa589b3a2f87b7a609c99a3a2febd9d
ChimeneBlokesch <chimene.blokesch2002@gmail.com>
2024-12-07 14:35:28 +0100

    get wikitext2 dataset based on example code from auto-gptq

--- autogptq.py
+++ autogptq.py
@@ -1,3 +1,8 @@
+"""
+Adapted from examples/quantization/basic_usage_wikitext2.py
+from the AutoGPTQ repo.
+"""
+
 from transformers import AutoTokenizer, AutoModelForCausalLM, GPTQConfig
 from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
 from auto_gptq.utils import Perplexity
@@ -15,20 +20,45 @@ def load_model(args) -> AutoGPTQForCausalLM:
 
     quantize_config = BaseQuantizeConfig(
         bits=args.wbits,
-        group_size=args.group_size,
-        dataset="wikitext2"
-    )
-
-    gptq_config = GPTQConfig(
-        bits=args.wbits,
-        dataset="wikitext2",
-        tokenizer=tokenizer
+        group_size=args.group_size
     )
 
     model = AutoGPTQForCausalLM.from_pretrained(
         args.model, quantization_config=quantize_config)
 
     return model, tokenizer
+
+
+def get_wikitext2(nsamples, seed, seqlen, model):
+    from datasets import load_dataset
+
+    traindata = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
+    testdata = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+
+    from transformers import AutoTokenizer
+
+    try:
+        tokenizer = AutoTokenizer.from_pretrained(model, use_fast=False)
+    except Exception:
+        tokenizer = AutoTokenizer.from_pretrained(model, use_fast=True)
+    trainenc = tokenizer("\n\n".join(traindata["text"]), return_tensors="pt")
+    testenc = tokenizer("\n\n".join(testdata["text"]), return_tensors="pt")
+
+    import random
+
+    random.seed(seed)
+    np.random.seed(0)
+    torch.random.manual_seed(0)
+
+    traindataset = []
+    for _ in range(nsamples):
+        i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
+        j = i + seqlen
+        inp = trainenc.input_ids[:, i:j]
+        attention_mask = torch.ones_like(inp)
+        traindataset.append(
+            {"input_ids": inp, "attention_mask": attention_mask})
+    return traindataset, testenc
 
 
 if __name__ == "__main__":
@@ -66,5 +96,8 @@ if __name__ == "__main__":
         seqlen=model.seqlen,
     )
 
-    model.quantize(testloader, use_triton=False)
+    traindataset, testenc = get_wikitext2(args.nsamples, args.seed,
+                                          model.seqlen, args.model)
+    model.quantize(traindataset, use_triton=False)
+    model.save_quantized(args.save_dir)
     model.save_quantized(args.save_dir, use_safetensors=True)
